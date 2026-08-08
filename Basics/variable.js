d4ab05ade27f0199b12d02d7516cd9883d151355
parnika0369 <parnika03066030@gmail.com>
2026-08-08 11:28:06 +0530

    Add variable.js

---
+++ Basics/variable.js
@@ -0,0 +1,16 @@
+const accountID = 14453
+let accountEmail = "Parnika@google.com"
+var accountpassword = "123456"
+accountCity = "jaipur"
+
+let accountState;
+
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountpassword,accountCity, accountState])
+
+/*
+prefer not to use var
+beacuse of issue in block scope of functional scope 
+use var
+*/
