86aaf2deeca7f2d3d23b89284bb9a8b402fb6f20
parnika0369 <parnika03066030@gmail.com>
2026-08-08 11:53:34 +0530

    added datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code(in this file ) as newer version of js
+// alert(3+3) //we r using nodejs not browser
+
+let name = "Parnika"
+let age = 9
+let isLoggedin = false
+
+//number 2^53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined
+//symbol
+//object
+
+
+console.log(typeof "Pariniti");
+console.log(typeof null);
+
+
